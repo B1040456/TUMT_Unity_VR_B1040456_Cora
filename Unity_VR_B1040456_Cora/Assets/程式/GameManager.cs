21a4e09de91026cf50dae0ba8b0becdf3e5c4708
rio19991229 <55218512+rio19991229@users.noreply.github.com>
2020-01-08 23:12:17 +0800

    vr

---
+++ Unity_VR_B1040456_Cora/Assets/程式/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool cube;
+
+    public void ThrowPickUp()
+    {
+        print("撿起物件");
+    }
+
+    public void ThrowDetach(GameObject obj)
+    {
+        print("放開物件");
+        cube = true;
+        Destroy(obj);
+    }
+
+    public void ThrowHeld()
+    {
+        print("拿著物件");
+    }
+}
---
+++ Unity_VR_B1040456_Cora/Assets/程式/teleportopen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class teleportopen : MonoBehaviour
+{
+    public GameObject teleport;
+
+    private void Update()
+    {
+        if (GameManager.cube)
+        {
+            teleport.SetActive(true);
+        }
+    }
+}
